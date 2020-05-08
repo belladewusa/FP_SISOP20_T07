54f077d16f86d5522cd3ddc547466dd31f814a3d
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-05-08 16:28:37 +0800

    Update mv.c

--- mv.c
+++ mv.c
@@ -1,35 +1,412 @@
-// digunakan untuk memindahkan file lama ke file baru 
+// memindahkan file ke folder
 // mv.c
 //T07
 
-#include "user.h" 
+#include "fcntl.h"
 
-#include "stat.h" 
+#include "user.h"
 
-#include "types.h" 
+#include "types.h"
+
+#include "fs.h"	 
+
+#include "stat.h"
 
 
+char buffer[512]; 
 
-int main (int argc, char *argv[])
+char* fmtname(char *path); 
+
+char* strcat(char *s1,char *s2); 
+
+
+void mv_all(char *awal, char *akhir); 	
+
+void mv(char *awal,char *akhir); 
+
+
+int main(int argc, char *argv[])
 
 {
-	
-  if (argc != 3)
-  
+
+  char *awal,*akhir; 
+
+  if(argc<=2)
+
   {
-  	
-    printf(2, "penggunaan: mv file_lama file_baru\n"); 
-    
+
+    printf(1," penggunaan: mv awal akhir\n"); 
+
     exit(); 
-    
+
   }
 
- if ((link(argv[1], argv[2]) < 0) || (unlink(argv[1]) < 0)) 
- // mengahapus file yang telah di pindahkan 
- 
-    printf(2, "mv %s to %s failed\n", argv[1], argv[2]); 
+  if(argc==3)
 
+  {
+
+    if(argv[1][0]=='*')
+
+    {
+
+      akhir=argv[2]; 
+
+      mv_all(".",akhir); 
+
+      exit();  
+
+    }
+
+    else
+
+    {
+
+        awal=argv[1]; 
+
+      akhir=argv[2]; 
+
+      mv(awal,akhir); 
+
+    }
+
+    exit(); 	
+
+  }
+
+  if(argc==4)
+
+  {
+
+    if(argv[1][0]=='*')
+
+    {
+
+
+      awal=argv[2]; 
+
+      akhir=argv[3]; 
+
+      mv_all(awal,akhir); 	
+
+    }
+
+    exit(); 
+
+  }
 
   exit(); 
+
+}
+
+
+
+char* fmtname(char *path)
+
+{
+
+  static char buf[DIRSIZ+1]; 
+
+  char *p; 
+
+
+  // Find first character after last slash.
+
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+
+    ; 
+
+  p++; 	
+
+
+
+  // Return blank-padded name.
+
+  if(strlen(p) >= DIRSIZ)
+
+    return p; 
+
+  memmove(buf, p, strlen(p)); 
+
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p)); 
+
+  return buf; 
+
+}
+
+
+
+char* strcat(char *s1,char *s2)
+
+{
+
+  char *b=s1; 
+
+  while(*s1)++s1; 
+
+  while(*s2)*s1++=*s2++; 
+
+  *s1=0; 
+
+  return b; 
+
+}
+
+
+
+void mv_all(char *awal, char *akhir)
+
+{
+
+  char buf[512], *p; 
+
+  int fd; 
+
+  struct dirent dr; 
+
+  struct stat st; 
+
+  
+
+  stat(awal,&st); 	
+	
+  fd=open(awal,O_RDONLY); 
+
+  
+
+  if(st.type==T_FILE)
+
+  {
+
+    printf(1,"%s is a file\n", awal);	
+
+    return; 
+
+  }
+
+
+
+  else if(st.type==T_DIR)
+
+  {
+
+    strcpy(buf, awal); 
+
+    p = buf+strlen(buf); 
+
+    *p++ = '/'; 
+
+    
+
+    while(read(fd, &dr, sizeof(dr)) == sizeof(dr))
+
+    {
+
+      if(dr.inum == 0)
+
+        continue; 
+      
+
+      memmove(p, dr.name, DIRSIZ); 
+
+      p[DIRSIZ] = 0; 
+
+      
+
+      if(stat(buf,&st)<0)
+
+	  {
+
+        printf(1,"mv *: file cannot be stats\n"); 
+
+        continue; 
+
+      }
+
+
+
+      if(st.type==1)
+
+      {
+
+        if(fmtname(buf)[0]=='.')
+
+        {
+
+          continue; 	
+
+        }
+
+        
+
+        char awals[512],dawal[512]; 
+
+  
+
+        //select source
+
+        strcpy(awals,awal); 
+
+        strcat(awals,"/"); 
+
+        strcat(awals,p); 
+
+        
+
+        //select dest
+
+        strcpy(dawal,akhir); 
+
+        strcat(dawal,"/"); 
+
+        strcat(dawal,p); 
+
+      
+
+      mkdir(dawal);
+
+        mv_all(awals,dawal); 
+
+  
+
+        unlink(awals); 
+
+      }
+
+      else
+
+      {        
+
+        char awals[512],dawal[512]; 
+
+      
+
+      //select source
+
+        strcpy(awals,awal); 
+
+        strcat(awals,"/"); 
+
+        strcat(awals,p); 
+
+        
+
+        //select dest
+
+        strcpy(dawal,akhir); 
+
+        strcat(dawal,"/"); 
+
+        strcat(dawal,p); 
+
+      
+
+      //domove
+
+      mv(awals,dawal); 
+
+        unlink(awals); 
+
+      } 
+
+    }
+
+  }
+
+  close(fd); 
+
+}
+
+
+
+void mv(char *awal,char *akhir)
+
+{
+
+  int fdawal, fdakhir, n,flag=1; 
+
+  char sec[512]; 
+
+  
+
+  fdawal=open(awal,O_RDWR); 
+
+  fdakhir=open(akhir,O_RDWR); 
+
+  
+
+  if(fdakhir>=0)
+
+  {
+
+    flag=0; 
+
+  }
+
+
+
+  close(fdakhir); 
+
+  fdakhir=open(akhir,O_RDWR | O_CREATE); 	
+
+
+
+  if(fdawal<0 && fdakhir>=0){
+
+    printf(1,"%s does not exist or is a directory\n",awal); 	
+
+    if(flag)
+
+    {
+
+      unlink(akhir); 
+
+    }
+
+
+
+    close(fdawal); 
+
+    close(fdakhir); 
+
+    return; 
+
+  }
+
+  else if(fdawal<0 && fdakhir<0)
+
+  {
+
+    close(fdawal); 
+
+    close(fdakhir); 
+
+    return; 
+
+  }
+
+  else if(fdakhir<0 && fdawal>=0)
+
+  {
+
+    close(fdakhir); 
+
+    fdakhir=open(sec,O_CREATE | O_RDWR); 	
+  }
+
+  
+
+  while((n=read(fdawal,buffer,sizeof(buffer)))>0) 
+
+    write(fdakhir,buffer,fdawal); 	
+
+  
+
+  unlink(awal); 
+
+  
+
+  close(fdawal); 	
+
+  close(fdakhir); 
 
 }
