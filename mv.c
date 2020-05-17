575accc59211b88a717f42795ee1b877a0a86402
belladewusa <55181780+belladewusa@users.noreply.github.com>
2020-05-17 21:04:41 +0800

    Update mv.c

--- mv.c
+++ mv.c
@@ -1,416 +1,105 @@
-// memindahkan file ke folder
-
-// mv.c
-
-//T07
-
-#include "fcntl.h" 	
-
-#include "user.h"	
-
+// Perbedaan antara kedua sintaksis ini halus tetapi penting. 
+//Jika file header disertakan dalam <>, preprocessor akan mencari jalur direktori yang telah ditentukan untuk menemukan file header. 
+//Jika file header dilampirkan dalam "", preprocessor akan mencari file header di direktori yang sama dengan file sumber.
 #include "types.h"
+//Berbagai TIPE DATA digunakan di tempat lain
+#include "stat.h"
+// <sys / stat.h> menentukan struktur data yang dikembalikan oleh fungsi fstat () , lstat () , dan stat ()
+//Fungsi stat() dan lstat() mengambil argumen nama file . Jika file tersebut merupakan tautan simbolis , stat() mengembalikan atribut dari target tautan,
+#include "user.h"
+//untuk GDB ,GDB adalah debugger gratis yang ditawarkan dari Proyek GNU
+#include "fcntl.h"
+//kontrol file
+#include "fs.h"
+// pembungkus sistem file
 
-#include "fs.h"	  
+//Void.Fungsi ini tidak mengembalikan nilai keluaran ( return output ) yang didapat dari hasil proses tersebut
 
-#include "stat.h" 
+//fungsi untuk memindahkan src ke dest
+void move(char *src, char *dest)
+    {
+      if (link(src, dest)!=0) //copy
+        {
+        printf(1,"Unable to move, file cant link or not found\n");
+        exit();
+        }
+      
+      if (unlink(src)!=0) //hapus
+        {
+        printf(1,"Unable to delete the old one\n");
+        exit();
+        }
+    }
 
+//membuka folder dan memindahkan file
+void aster(char *dest)
+    {
+        //deklarasi
+        int fd= open(".",O_RDONLY),cek;  //buka dir, cek
+        int lndest= strlen(dest); // cek jumlah karakter(string) di dir
+        struct stat st; // mengambil argumen nama file , mengembalikan atribut dari tautan target = st
+        struct dirent de; //direktori yg dimasuki = de
+        char buf[512]; // alokasi buffer 512 byte = 64 bit
 
-char buffer[512]; 
-
-char* fmtname(char *path); 
-
-char* strcat(char *s1,char *s2); 
-
-
-void mv_all(char *awal, char *akhir); 	
-
-void mv(char *awal,char *akhir); 
-
+        //mmenambahkan/memindahkan  file ke folder  
+         while(read(fd, &de, sizeof(de)) == sizeof(de)) 
+            {
+            cek= stat(de.name, &st);  
+            if(cek == 0 && st.type == T_FILE) 
+                {
+                strcpy(buf, dest);
+                strcpy(&buf[lndest],"/"); 
+                strcpy(&buf[lndest+1], de.name);
+                move(de.name,buf);
+                }
+            }
+    }
 
 int main(int argc, char *argv[])
-
-{
-
-  char *awal,*akhir; 
-
-  if(argc<=2)
-
-  {
-
-    printf(1," penggunaan: mv awal akhir\n"); 
-
-    exit(); 
-
-  }
-
-  if(argc==3)
-
-  {
-
-    if(argv[1][0]=='*')
-
     {
-
-      akhir=argv[2]; 
-
-      mv_all(".",akhir); 
-
-      exit();  
-
-    }
-
-    else
-
-    {
-
-        awal=argv[1]; 
-
-      akhir=argv[2]; 
-
-      mv(awal,akhir); 
-
-    }
-
-    exit(); 	
-
-  }
-
-  if(argc==4)
-
-  {
-
-    if(argv[1][0]=='*')
-
-    {
-
-
-      awal=argv[2]; 
-
-      akhir=argv[3]; 
-
-      mv_all(awal,akhir); 	
-
-    }
-
-    exit(); 
-
-  }
-
-  exit(); 
-
-}
-
-
-
-char* fmtname(char *path)
-
-{
-
-  static char buf[DIRSIZ+1]; 
-
-  char *p; 
-
-
-// Temukan karakter pertama setelah slash terakhir.
-
-  for(p=path+strlen(path); p >= path && *p != '/'; p--)
-
-    ; 
-
-  p++; 	
-
-
-
-  // kembalikan blank-padded name.
-
-  if(strlen(p) >= DIRSIZ)
-
-    return p; 
-
-  memmove(buf, p, strlen(p)); 
-
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p)); 
-
-  return buf; 
-
-}
-
-
-
-char* strcat(char *s1,char *s2)
-
-{
-
-  char *b=s1; 
-
-  while(*s1)++s1; 
-
-  while(*s2)*s1++=*s2++; 
-
-  *s1=0; 
-
-  return b; 
-
-}
-
-
-
-void mv_all(char *awal, char *akhir)
-
-{
-
-  char buf[512], *p; 
-
-  int fd; 
-
-  struct dirent dr; 
-
-  struct stat st; 
-
-  
-
-  stat(awal,&st); 	
-	
-  fd=open(awal,O_RDONLY); 
-
-  
-
-  if(st.type==T_FILE)
-
-  {
-
-    printf(1,"%s is a file\n", awal);	
-
-    return; 
-
-  }
-
-
-
-  else if(st.type==T_DIR)
-
-  {
-
-    strcpy(buf, awal); 
-
-    p = buf+strlen(buf); 
-
-    *p++ = '/'; 
-
-    
-
-    while(read(fd, &dr, sizeof(dr)) == sizeof(dr))
-
-    {
-
-      if(dr.inum == 0)
-
-        continue; 
-      
-
-      memmove(p, dr.name, DIRSIZ); 
-
-      p[DIRSIZ] = 0; 
-
-      
-
-      if(stat(buf,&st)<0)
-
-	  {
-
-        printf(1,"mv *: file cannot be stats\n"); 
-
-        continue; 
-
-      }
-
-
-
-      if(st.type==1)
-
-      {
-
-        if(fmtname(buf)[0]=='.')
-
-        {
-
-          continue; 	
-
+        //deklarasi variable
+        int cek,i,lndest,fd;
+        char buf[512];
+        struct stat st;
+
+//jika tidak ada argumen     / proses argumen   
+        if((fd = open(argv[argc-1],O_RDONLY))<0) //buka folder, read only
+        {       
+        cek=stat(argv[argc-1],&st); 
+        lndest=strlen(argv[argc-1]);
         }
-
         
+//jika mengetik mv saja akan keluar info cara penggunaan 
+        if (argc<2)
+            {
+            printf(1,"Usage: mv file dest\n");
+            }
+    
+//berhasil dipindahkan
+        if(strcmp(argv[1],"*")==0) //perbandingan, untuk cek file ada
+                {
+                aster(argv[2]); //akan menjalankan fungsi aster
+                exit();
+                }
 
-        char awals[512],dawal[512]; 
-
-  
-
-        //select awal
-
-        strcpy(awals,awal); 
-
-        strcat(awals,"/"); 
-
-        strcat(awals,p); 
-
+//cek jika dest bukan direktori     
+        if(cek==0 && st.type != T_DIR) 
+            {
+            printf(1,"Dest %s is not a directory.\n",argv[argc-1]);
+            exit();
+            }
+    
+//link argumen ke sebuah direktori
+        for(i=1; i<argc-1; i++)
         
-
-        //select akhir
-
-        strcpy(dawal,akhir); 
-
-        strcat(dawal,"/"); 
-
-        strcat(dawal,p); 
-
-      
-
-      mkdir(dawal);
-
-        mv_all(awals,dawal); 
-
-  
-
-        unlink(awals); 
-
-      }
-
-      else
-
-      {        
-
-        char awals[512],dawal[512]; 
-
-      
-
-      //select awal
-
-        strcpy(awals,awal); 
-
-        strcat(awals,"/"); 
-
-        strcat(awals,p); 
-
-        
-
-        //select akhir
-
-        strcpy(dawal,akhir); 
-
-        strcat(dawal,"/"); 
-
-        strcat(dawal,p); 
-
-      
-
-      //melakukan pemindahan 
-
-      mv(awals,dawal); 
-
-        unlink(awals); 
-
-      } 
-
-    }
-
-  }
-
-  close(fd); 
-
-}
-
-
-
-void mv(char *awal,char *akhir)
-
-{
-
-  int fdawal, fdakhir, n,flag=1; 
-
-  char sec[512]; 
-
-  
-
-  fdawal=open(awal,O_RDWR); 
-
-  fdakhir=open(akhir,O_RDWR); 
-
-  
-
-  if(fdakhir>=0)
-
-  {
-
-    flag=0; 
-
-  }
-
-
-
-  close(fdakhir); 
-
-  fdakhir=open(akhir,O_RDWR | O_CREATE); 	
-
-
-
-  if(fdawal<0 && fdakhir>=0){
-
-    printf(1,"%s does not exist or is a directory\n",awal); 	
-
-    if(flag)
-
-    {
-
-      unlink(akhir); 
-
+// menambahkan dari nama file ke nama direktori         {
+            strcpy(buf,argv[argc-1]);
+            strcpy(&buf[lndest],"/");
+            strcpy(&buf[lndest+1],argv[i]);
+            move(argv[i],buf);       
+        }
+            
+        exit(); 
     }
 
 
-
-    close(fdawal); 
-
-    close(fdakhir); 
-
-    return; 
-
-  }
-
-  else if(fdawal<0 && fdakhir<0)
-
-  {
-
-    close(fdawal); 
-
-    close(fdakhir); 
-
-    return; 
-
-  }
-
-  else if(fdakhir<0 && fdawal>=0)
-
-  {
-
-    close(fdakhir); 
-
-    fdakhir=open(sec,O_CREATE | O_RDWR); 	
-  }
-
-  
-
-  while((n=read(fdawal,buffer,sizeof(buffer)))>0) 
-
-    write(fdakhir,buffer,fdawal); 	
-
-  
-
-  unlink(awal); 
-
-  
-
-  close(fdawal); 	
-
-  close(fdakhir); 
-
-}
-
-//FP20
